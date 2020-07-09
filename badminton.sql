51c8ae379a26f03a228c6f23e6ff33d2a7e641ce
Li Ian-Xue (b4283) <b4283@pm.me>
2020-07-10 02:45:02 +0800

    Add ranking.

--- badminton.sql
+++ badminton.sql
@@ -1,16 +1,23 @@
 CREATE TABLE team_player (id integer primary key AUTOINCREMENT, team_id integer, player_id integer);
 CREATE TABLE player (id integer primary key AUTOINCREMENT, name varchar);
 CREATE TABLE game (id integer primary key AUTOINCREMENT );
-CREATE TABLE game_team_score (id integer primary key AUTOINCREMENT, game_id integer, team_id integer, team_score integer);
 CREATE TABLE team (id integer PRIMARY KEY AUTOINCREMENT );
-create view all_games (id, game_id, ta_p1, ta_p2, ta_score, tb_p1, tb_p2, tb_score) as
+CREATE TABLE game_team_score (id integer primary key AUTOINCREMENT, game_id integer, team_id integer, team_score integer);
+CREATE VIEW all_games (id, game_id, ta_p1, ta_p2, ta_score, tb_p1, tb_p2, tb_score) as
 select t1g.id, t1g.game_id, a1.name, a2.name, t1g.team_score, b1.name, b2.name, t2g.team_score
   from player a1, player a2, player b1, player b2,
     team_player a1t, team_player a2t, team_player b1t, team_player b2t,
     game_team_score t1g, game_team_score t2g
   where
-      a1t.player_id = a1.id and a2t.player_id = a2.id and a1t.team_id = a2t.team_id and a1t.player_id < a2t.player_id
+        a1t.player_id = a1.id and a2t.player_id = a2.id and a1t.team_id = a2t.team_id and a1t.player_id < a2t.player_id
     and b1t.player_id = b1.id and b2t.player_id = b2.id and b1t.team_id = b2t.team_id and b1t.player_id < b2t.player_id
     and t1g.team_id = a1t.team_id and t2g.team_id = b1t.team_id
     and t1g.team_id < t2g.team_id
     and t1g.game_id = t2g.game_id;
+CREATE VIEW player_score_sum as
+  select a.name, sum(c.team_score) score
+    from player a, team_player b, game_team_score c
+    where
+  b.player_id = a.id and c.team_id = b.team_id
+  group by a.id
+  order by score desc;
--- index.cgi
+++ index.cgi
@@ -5,16 +5,28 @@ set -euo pipefail
 echo -ne "Content-type: text/html\n\n"
 
 readarray -t GAMES <<< $(sqlite3 badminton.db "select id, game_id, ta_p1, ta_p2, ta_score, tb_p1, tb_p2, tb_score from all_games;")
+readarray -t RANKINGS <<< $(sqlite3 badminton.db "select name, score from player_score_sum;")
 
 function generate_games_table {
     for GAME in "${GAMES[@]}"; do
-        readarray -d \| -t GAME_DETAIL <<< "$(echo "$GAME")" 
+        readarray -d \| -t GAME_DETAIL <<< "$(echo "$GAME")"
         echo "<tr>"
         echo "<td><input type=\"radio\" name=\"DELETE_ID\" value=\"${GAME_DETAIL[0]}\" /></td>"
         for i in $(seq 1 7); do
             echo "<td>${GAME_DETAIL[$i]}</td>"
-        done 
-        echo "</tr>" 
+        done
+        echo "</tr>"
+    done
+}
+
+function generate_ranking {
+    for RANK in "${RANKINGS[@]}"; do
+        readarray -d \| -t RANK_DETAIL <<< "$(echo "$RANK")"
+        echo "<tr>"
+        for i in $(seq 0 1); do
+            echo "<td>${RANK_DETAIL[$i]}</td>"
+        done
+        echo "</tr>"
     done
 }
 
@@ -27,6 +39,12 @@ cat <<EOF
     <table border="1">
     <tr><th></th><th>GAME #</th><th>P1</th><th>P2</th><th>TEAM A</th><th>P1</th><th>P2</th><th>TEAM B</th></tr>
     $(generate_games_table)
+    </table>
+
+    <h1>RANKING</h1>
+    <table border="1">
+    <tr><th>PLAYER</th><th>SCORE</th></tr>
+    $(generate_ranking)
     </table>
   </body>
 </html>
